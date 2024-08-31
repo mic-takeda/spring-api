acc6e208e611d9d34b21673b8ddafee01cd8e3c9
mic-takeda <mic-takeda@inter-mic.co.jp>
2024-08-31 14:28:06 +0900

    rest_mapping

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String getHello() {
         return "HelloWorld!!!!";
     }
